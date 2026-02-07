df5a6fc23bd1315900b5a32907b5a9421cda7e10
Juan Morais <timrodz@icloud.com>
2026-02-07 14:35:43 +1300

    refactor: Theme + favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Oxanium as FontSans, Geist_Mono as FontMono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
+const fontSans = FontSans({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
+const fontMono = FontMono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
@@ -25,9 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${fontSans.variable} ${fontMono.variable} antialiased`}>
         {children}
       </body>
     </html>
